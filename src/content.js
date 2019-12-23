870cf9def50faeca082e9f79087becb8e1b8dd2c
k8shiro <koheiyatsushiro@yahoo.co.jp>
2019-12-23 13:09:24 +0900

    wait slack dom

--- src/content.js
+++ src/content.js
@@ -31,25 +31,48 @@ function addLine(lineId, lineUrl) {
     element.setAttribute('class', 'element');
     element.innerHTML = `<iframe src="${lineUrl}" id="${lineId}">`
 
-    document.getElementById("wrapper").appendChild(element)
-    document.getElementById(lineId).addEventListener("load", function(){ iframeLoaded(lineId) });
+    document.getElementById("wrapper").appendChild(element);
+    document.getElementById(lineId).addEventListener("load", () => { iframeLoaded(lineId) });
 
-    document.getElementsByClassName("p-client")[0].style.width = '100%';
+    //document.getElementsByClassName("p-client")[0].style.width = '100%';
     let originWidth = 100 / (lines.length + 2) * 2;
     let wrapperWidth = 100 / (lines.length + 2) * lines.length;
-    document.getElementById("origin").style.width = String(originWidth) + '%'
-    document.getElementById("wrapper").style.width = String(wrapperWidth) + '%'
+    document.getElementById("origin").style.width = String(originWidth) + '%';
+    document.getElementById("wrapper").style.width = String(wrapperWidth) + '%';
 
     lineWidth = 100 / (lines.length);
     for (let element of document.getElementsByClassName('element')) {
-        element.style.width = String(lineWidth) + '%'
+        element.style.width = String(lineWidth) + '%';
     }
 
+    fixSlackDom();
+}
+
+function fixSlackDom() {
+    Promise.wait = (time) => new Promise(resolve => setTimeout(resolve, time || 0));
+    Promise.retry = (fn, tries, delay) => fn().catch(err => tries > 0 ? Promise.wait(delay).then(() => Promise.retry(fn, tries - 1, delay)) : Promise.reject('failed'));
+
+    let delay = 1000;
+    let tries = 10;
+    Promise.retry(()=>{
+        return new Promise((resolve, reject) => {
+            document.getElementsByClassName("p-client")[0].style.width = '100%';
+        })
+    }, tries, delay);
 }
 
 function iframeLoaded(lineId) {
-    data1 = document.getElementById(lineId);
-    data1.contentWindow.document.getElementsByClassName('p-classic_nav__team_header')[0].style.display = "none";
-    data1.contentWindow.document.getElementsByClassName('p-workspace__sidebar')[0].style.display = "none";
-    data1.contentWindow.document.getElementsByClassName('p-workspace--context-pane-collapsed')[0].style.gridTemplateColumns = "0px auto"
+    Promise.wait = (time) => new Promise(resolve => setTimeout(resolve, time || 0));
+    Promise.retry = (fn, tries, delay) => fn().catch(err => tries > 0 ? Promise.wait(delay).then(() => Promise.retry(fn, tries - 1, delay)) : Promise.reject('failed'));
+
+    let tries = 10;
+    let delay = 1000;
+    Promise.retry(()=>{
+        return new Promise((resolve, reject) => {
+            data1 = document.getElementById(lineId);
+            data1.contentWindow.document.getElementsByClassName('p-classic_nav__team_header')[0].style.display = "none";
+            data1.contentWindow.document.getElementsByClassName('p-workspace__sidebar')[0].style.display = "none";
+            data1.contentWindow.document.getElementsByClassName('p-workspace--context-pane-collapsed')[0].style.gridTemplateColumns = "0px auto";
+        })
+    }, tries, delay);
 }
